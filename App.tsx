94b8b8efae369220459b387c11827885c6191e9a
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-11-28 15:04:55 -0300

    (chore): initial configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { verifyInstallation } from "nativewind";
+import { StyleSheet, Text, View } from "react-native";
+import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
 
 export default function App() {
+  verifyInstallation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      <View className="w-screen flex-1 bg-red-400 items-center content-center">
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
