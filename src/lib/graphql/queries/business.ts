7ce9cd3ec7288c695dd2b7a30644ee73656036f6
Allyson Athyrson <allysonathyrson.work@gmail.com>
2026-03-01 22:54:35 -0300

    feat(frontend): load online and local baazars from GraphQL

--- src/lib/graphql/queries/business.ts
+++ src/lib/graphql/queries/business.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const GET_STORE_TESTE = gql`
+export const GET_LOCAL_BAAZARS = gql`
   query GetLocalBaazars {
     findAllLocalBaazars {
       id
@@ -11,6 +11,31 @@ export const GET_STORE_TESTE = gql`
       isOnline
       evaluations
       address
+      itemsType
+      locationMap {
+        id
+        latitude
+        longitude
+      }
+    }
+  }
+`;
+
+export const GET_ONLINE_BAAZARS = gql`
+  query GetOnlineBaazars {
+    findAllOnlineBaazars {
+      id
+      name
+      logoImage
+      averagePrice
+      evaluations
+      isOnline
+      address
+      description
+      images
+      linkInstagram
+      linkWhatsapp
+      openingHours
       itemsType
       locationMap {
         id
